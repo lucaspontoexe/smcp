84504454ea44d3ff8412a47b614791bb7bb21d7a
Lucas Teixeira <ltx16@hotmail.com>
2017-02-07 11:55:42 -0200

    Enviando arquivos

---
+++ ATmega_0.5.1_final_easteregg/ATmega_0.5.1_final_easteregg.ino
@@ -0,0 +1,145 @@
+//NOTA: TROCAR O SERIAL PRA TESTAR
+//OBRIGADO.
+//E agora, o CI não repete mais o que é enviado.
+
+
+#include "EmonLib.h"
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+
+const byte    numChars        = 48;
+char          receivedChars[numChars];  // an array to store the received data
+boolean       newData         = false;
+unsigned long previousMillis  = 0;      // will store last time LED was updated
+unsigned long previousMillis1 = 0;
+const long    interval        = 200;    // interval at which to blink (milliseconds)
+String        esquema;
+boolean       ocupado         = true;
+
+const int     pino_sct        = 0;
+const int     botao           = 11;
+int           estadoBotaoIP;
+int           tensao          = 220;
+int           calibra         = 18;
+String        ip = "(conectando)";
+String        dados;
+String        t, cal;
+
+EnergyMonitor emon1;
+SoftwareSerial esp8266(9, 10);
+LiquidCrystal lcd(4, 3, 5, 6, 7, 8);
+
+#include "easteregg.h"
+
+void recvWithEndMarker() {
+  static byte ndx = 0;
+  char endMarker = '\n';
+  char rc;
+
+  while (esp8266.available() > 0 && newData == false) {
+    rc = esp8266.read();
+
+    if (rc != endMarker) {
+      receivedChars[ndx] = rc;
+      ndx++;
+      if (ndx >= numChars) {
+        ndx = numChars - 1;
+      }
+    }
+    else {
+      receivedChars[ndx] = '\0'; // terminate the string
+      ndx = 0;
+      newData = true;
+    }
+  }
+}
+
+void showNewData() {
+  if (newData == true) {
+    //esp8266.print("This just in ... ");
+    //esp8266.println(receivedChars);
+    esquema = receivedChars;
+    newData = false;
+  }
+}
+
+void setup() {
+  pinMode (botao, INPUT);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0); lcd.print("SMCP      0.5.1");
+  lcd.setCursor(0, 1); lcd.print("Iniciando... ");
+  emon1.current(pino_sct, calibra);
+  esp8266.begin(9600);
+  esp8266.println("Tá ligado, tá ligado?");
+  ocupado = false;
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  double Irms = emon1.calcIrms(1480);
+  double potencia = Irms * tensao;
+  recvWithEndMarker();
+  showNewData();
+
+  if (esquema.indexOf("DADOS?") != -1) {
+    dados = "DADOS:{ \"corrente\": ";
+    dados += Irms;
+    dados += ", \"potencia\":";
+    dados += potencia;
+    dados += " }";
+    esp8266.println(dados);
+  }
+  if (esquema.indexOf("T:") != -1) {
+    t = esquema.substring(esquema.indexOf("T:") + 2, esquema.indexOf('\r'));
+    esp8266.print("Tensão ajustada para ");
+    esp8266.println(t);
+    tensao = t.toInt();
+  }
+  if (esquema.indexOf("CALIBRA:") != -1) {
+    cal = esquema.substring(esquema.indexOf("CALIBRA:") + 8, esquema.indexOf('\r'));
+    esp8266.print("Nível de calibração ajustado para: ");
+    esp8266.println(calibra);
+    calibra = cal.toInt();
+  }
+
+  if (esquema.indexOf("EASTEREGG") != -1) {
+  EASTEREGG();
+  }
+  estadoBotaoIP = digitalRead(botao);
+  if (estadoBotaoIP == HIGH) {
+    lcd.clear();
+    esp8266.println("IP?");
+    delay(150);
+    recvWithEndMarker();
+    showNewData();
+    if (esquema.indexOf("IP:") != -1) {
+      ip = esquema.substring(esquema.indexOf("IP:") + 3, esquema.indexOf('\r'));
+      ocupado = true;
+      lcd.clear(); //Apaga a tela assim que achar o IP.
+    }
+    unsigned long previousMillis1 = millis();
+    unsigned long currentMillis1 = millis();
+    while (currentMillis1 - previousMillis1 <= 3000) {
+      currentMillis1 = millis();
+      lcd.setCursor(0, 0); lcd.print("IP:          "); //Não repita tamanha gambiarra em casa.
+      lcd.setCursor(0, 1); lcd.print(ip);
+      recvWithEndMarker();
+      showNewData();
+    }
+  }
+  ip = "(desconectado)";
+  ocupado = false;
+  esquema = "";
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    if (ocupado == false) {
+      lcd.setCursor(0, 0);  lcd.print("Corr.(A): ");
+      lcd.setCursor(0, 1);  lcd.print("Pot. (W): ");
+      lcd.setCursor(10, 0); lcd.print(Irms);
+      lcd.setCursor(10, 1); lcd.print(potencia, 1);
+    }
+  }
+}
---
+++ ATmega_0.5.1_final_easteregg/easteregg.h
@@ -0,0 +1,110 @@
+#ifndef _EASTEREGG_H
+#define _EASTEREGG_H
+
+void b(int ba, int bb, int bc) {
+  float tempo =  ba * 461.5;
+  tempo += bb * 230.75;
+  tempo += bc * 115.375;
+  delay(tempo);
+}
+
+void tali() {
+  esp8266.println("Easter Egg tá na tela");
+  for (int i = 0; i < 2; i++) {
+    lcd.setCursor(0, 0); lcd.print("TALI");
+    b(1, 0, 0);
+    lcd.setCursor(5, 0); lcd.print("TALI");
+    b(1, 0, 0);
+    lcd.setCursor(5, 1); lcd.print("TA LIGADO");
+    b(2, 0, 0);
+    lcd.clear();
+    lcd.setCursor(1, 0); lcd.print("QUE NAO E PRA");
+    lcd.setCursor(2, 1); lcd.print("QUALQUER UM");
+    b(4, 0, 0);
+    lcd.clear();
+    lcd.setCursor(0, 0); lcd.print("SMCP ATUALIZADO");
+    lcd.setCursor(3, 1); lcd.print("E O 0.5.1");
+    b(8, 0, 0);
+    lcd.clear();
+  }
+}
+
+void cemporcento() {
+  lcd.clear();
+  lcd.setCursor(0, 0); lcd.print("100% ATUALIZADO");
+  b(4, 1, 0);
+  lcd.clear();
+  lcd.setCursor(0, 0); lcd.print("E RUIM DE ATURAR");
+  b(3, 1, 0);
+  lcd.clear();
+  lcd.setCursor(0, 0); lcd.print("SMCP VIROU MODA");
+  b(4, 0, 0);
+  lcd.clear();
+  lcd.setCursor(3, 0); lcd.print("TODO MUNDO");
+  lcd.setCursor(3, 1); lcd.print("QUER USAR");
+  b(4, 1, 0);
+
+  lcd.clear();
+  lcd.setCursor(5, 0); lcd.print("COM A");
+  lcd.setCursor(2, 1); lcd.print("NOSSA EQUIPE");
+  b(4, 0, 0);
+  lcd.clear();
+  lcd.setCursor(2, 0); lcd.print("NINGUEM BATE");
+  lcd.setCursor(3, 1); lcd.print("DE FRENTE");
+  b(3, 1, 0);
+  lcd.clear();
+  lcd.setCursor(0, 0); lcd.print("SMCP E NERVOSO");
+  b(4, 0, 0);
+  lcd.clear();
+  lcd.setCursor(2, 0); lcd.print("NAO DA CHANCE");
+  lcd.setCursor(0, 1); lcd.print("AO CONCORRENTE");
+  b(4, 1, 0);
+
+  lcd.clear();
+  lcd.setCursor(0, 0); lcd.print("SE LIGA AI MANO");
+  b(3, 1, 0);
+  lcd.clear();
+  lcd.setCursor(0, 0); lcd.print("ESCUTE O QUE EU");
+  lcd.setCursor(3, 1); lcd.print("VOU DIZER");
+  b(4, 0, 0);
+  lcd.clear();
+  lcd.setCursor(3, 0); lcd.print("QUEM FECHA");
+  lcd.setCursor(4, 1); lcd.print("COM SMCP");
+  b(4, 0, 0);
+  lcd.clear();
+  lcd.setCursor(3, 0); lcd.print("SEMPRE VAI");
+  lcd.setCursor(3, 1); lcd.print("FORTALECER");
+  b(4, 0, 0);
+  lcd.clear();
+
+  lcd.setCursor(2, 0); lcd.print("PORQUE TUDO");
+  lcd.setCursor(3, 1); lcd.print("QUE E BOM");
+  b(5, 0, 0);
+  lcd.clear();
+  lcd.setCursor(1, 0); lcd.print("A GENTE REPETE");
+  b(3, 0, 0);
+  lcd.clear();
+  lcd.setCursor(3, 0); lcd.print("E POR ISSO");
+  lcd.setCursor(2, 1); lcd.print("QUE EU FECHO");
+  b(4, 0, 0);
+  lcd.clear();
+  lcd.setCursor(2, 0); lcd.print("COM A EQUIPE");
+  lcd.setCursor(6, 1); lcd.print("SMCP");
+  b(4, 0, 0);
+  lcd.clear();
+}
+
+void EASTEREGG() {
+    ocupado = true;
+    lcd.clear();
+    b(1, 0, 0);
+    tali();
+    cemporcento();
+    tali();
+    lcd.setCursor(4, 0); lcd.print("samples");
+    lcd.setCursor(0, 1); lcd.print("incompreensiveis");
+    b(16, 0, 0);
+    lcd.clear();
+    ocupado = false;
+}
+#endif

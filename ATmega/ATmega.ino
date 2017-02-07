1332c0256483e1cb3f00bdd2906b2e2c9a1e03ac
Lucas Teixeira <ltx16@hotmail.com>
2017-02-07 13:45:45 -0200

    Informalidades removidas
    Menos gírias e mais explicações

--- ATmega/ATmega.ino
+++ ATmega/ATmega.ino
@@ -1,20 +1,27 @@
-//NOTA: TROCAR O SERIAL PRA TESTAR
+//NOTA: TROCAR O SERIAL QUANDO FOR TESTAR
 //OBRIGADO.
-//E agora, o CI não repete mais o que é enviado.
 
+/*  PARTES IMPORTANTES:
+ *   
+ *   Linha | Parte
+ *      14 | Declaração de variáveis
+ *      39 | Comunicação serial
+ *      73 | Configurações
+ *      85 | Loop
+ */    
 
 #include "EmonLib.h"
 #include <LiquidCrystal.h>
 #include <SoftwareSerial.h>
 
-const byte    numChars        = 48;
-char          receivedChars[numChars];  // an array to store the received data
-boolean       newData         = false;
-unsigned long previousMillis  = 0;      // will store last time LED was updated
-unsigned long previousMillis1 = 0;
-const long    interval        = 200;    // interval at which to blink (milliseconds)
-String        esquema;
-boolean       ocupado         = true;
+const byte    numChars        = 48;     // O programa só recebe 48 caracteres de cada vez
+char          receivedChars[numChars];  // Array pra guardar os dados enviados por serial
+boolean       newData         = false;  // Muda quando há dados novos pelo serial
+unsigned long previousMillis  = 0;      // Salva a última vez em que o display foi atualizado
+unsigned long previousMillis1 = 0;      // Usado para contar o tempo em que o IP fica no display
+const long    interval        = 200;    // Intervalo de atualização do display
+String        comandos;                 // Mesma coisa que o array receivedChars, porém em formato de string.
+boolean       ocupado         = true;   // Usada em outra versão, pode ser excluída.
 
 const int     pino_sct        = 0;
 const int     botao           = 11;
@@ -31,6 +38,7 @@ LiquidCrystal lcd(4, 3, 5, 6, 7, 8);
 
 #include "easteregg.h"
 
+//Comunicação serial
 void recvWithEndMarker() {
   static byte ndx = 0;
   char endMarker = '\n';
@@ -47,7 +55,7 @@ void recvWithEndMarker() {
       }
     }
     else {
-      receivedChars[ndx] = '\0'; // terminate the string
+      receivedChars[ndx] = '\0'; // Termina a string
       ndx = 0;
       newData = true;
     }
@@ -56,13 +64,12 @@ void recvWithEndMarker() {
 
 void showNewData() {
   if (newData == true) {
-    //esp8266.print("This just in ... ");
-    //esp8266.println(receivedChars);
-    esquema = receivedChars;
+    comandos = receivedChars;
     newData = false;
   }
 }
 
+//Configurações
 void setup() {
   pinMode (botao, INPUT);
   lcd.begin(16, 2);
@@ -70,10 +77,11 @@ void setup() {
   lcd.setCursor(0, 1); lcd.print("Iniciando... ");
   emon1.current(pino_sct, calibra);
   esp8266.begin(9600);
-  esp8266.println("Tá ligado, tá ligado?");
+  esp8266.println("Ligado");
   ocupado = false;
 }
 
+//Loop
 void loop() {
   unsigned long currentMillis = millis();
   double Irms = emon1.calcIrms(1480);
@@ -81,7 +89,7 @@ void loop() {
   recvWithEndMarker();
   showNewData();
 
-  if (esquema.indexOf("DADOS?") != -1) {
+  if (comandos.indexOf("DADOS?") != -1) {
     dados = "DADOS:{ \"corrente\": ";
     dados += Irms;
     dados += ", \"potencia\":";
@@ -89,21 +97,21 @@ void loop() {
     dados += " }";
     esp8266.println(dados);
   }
-  if (esquema.indexOf("T:") != -1) {
-    t = esquema.substring(esquema.indexOf("T:") + 2, esquema.indexOf('\r'));
+  if (comandos.indexOf("T:") != -1) {
+    t = comandos.substring(comandos.indexOf("T:") + 2, comandos.indexOf('\r'));
     esp8266.print("Tensão ajustada para ");
     esp8266.println(t);
     tensao = t.toInt();
   }
-  if (esquema.indexOf("CALIBRA:") != -1) {
-    cal = esquema.substring(esquema.indexOf("CALIBRA:") + 8, esquema.indexOf('\r'));
+  if (comandos.indexOf("CALIBRA:") != -1) {
+    cal = comandos.substring(comandos.indexOf("CALIBRA:") + 8, comandos.indexOf('\r'));
     esp8266.print("Nível de calibração ajustado para: ");
     esp8266.println(calibra);
     calibra = cal.toInt();
   }
 
-  if (esquema.indexOf("EASTEREGG") != -1) {
-  EASTEREGG();
+  if (comandos.indexOf("EASTEREGG") != -1) {
+    EASTEREGG();
   }
   estadoBotaoIP = digitalRead(botao);
   if (estadoBotaoIP == HIGH) {
@@ -112,8 +120,8 @@ void loop() {
     delay(150);
     recvWithEndMarker();
     showNewData();
-    if (esquema.indexOf("IP:") != -1) {
-      ip = esquema.substring(esquema.indexOf("IP:") + 3, esquema.indexOf('\r'));
+    if (comandos.indexOf("IP:") != -1) {
+      ip = comandos.substring(comandos.indexOf("IP:") + 3, comandos.indexOf('\r'));
       ocupado = true;
       lcd.clear(); //Apaga a tela assim que achar o IP.
     }
@@ -121,7 +129,7 @@ void loop() {
     unsigned long currentMillis1 = millis();
     while (currentMillis1 - previousMillis1 <= 3000) {
       currentMillis1 = millis();
-      lcd.setCursor(0, 0); lcd.print("IP:          "); //Não repita tamanha gambiarra em casa.
+      lcd.setCursor(0, 0); lcd.print("IP:          "); 
       lcd.setCursor(0, 1); lcd.print(ip);
       recvWithEndMarker();
       showNewData();
@@ -129,10 +137,10 @@ void loop() {
   }
   ip = "(desconectado)";
   ocupado = false;
-  esquema = "";
+  comandos = "";
 
   if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
+    // Salva a última vez em que o display foi atualizado
     previousMillis = currentMillis;
 
     if (ocupado == false) {
